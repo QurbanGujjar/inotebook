1033fb85a8be11b03f459a9cc22a7e5324c35caa
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2022-04-09 17:38:09 +0500

    inotebook compleated

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,12 @@ const connectToMongo = () => {
     });
 }
 
-
-
 module.exports = connectToMongo;
+
+
+// This happened probably because the MongoDB service isn't started. Follow the below steps to start it:
+
+// Go to Control Panel and click on Administrative Tools.
+// Double click on Services. A new window opens up.
+// Search MongoDB.exe. Right click on it and select Start.
+// The server will start. Now execute npm start again and the code might work this time.
